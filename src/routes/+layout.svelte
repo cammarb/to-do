d345df28e2ca151160491462315b3c6f40017374
Camila Martinez <camilamarb98@gmail.com>
2023-03-23 14:25:13 +0100

    Update Structure and Styles:
    - Add Login, Register and Account pages
    - Add nav element to the project's layout
    - Add default sizes for heading tags in src/app.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,19 @@
     import "../app.css";
 </script>
 
-<div class="">
+<nav class="px-2 sm:px-4 py-2.5 sticky w-full z-20 top-0 left-0 border-b bg-white border-gray-200 text-neutral-900
+ dark:bg-neutral-900 dark:border-gray-600 dark:text-neutral-200">
+  <div class="logo">
+    <a href="/">
+        <img src="%sveltekit.assets%/favicon.png" alt="To-Do website logo">
+    </a>
+  </div>
+  <div class="nav-items">
+
+  </div>
+</nav>
+  
+
+<div class="container px-4 py-3">
     <slot />
 </div>
